8d19f111029ee2d0edd1c1b42b53929462553d62
El Heroe <116875322+Liya-23@users.noreply.github.com>
2023-07-25 15:41:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+#ask the user what temperature they want to convert
 UnitType = input("What Unit is The Temperature in? (F/C)")
-if UnitType == "C" or UnitType == "c" :
+
+#if it happens to be in Celcius it must be converted to Fahrenheit
+if UnitType.upper() == "C":
     Celsius_input = input("What Degree Celsius is the Temperature? ")
     Celsius_input = int(Celsius_input)
+    #display which type of temperature we are converting to
     print("Converting to Fahrenheit!!!")
+    #conversion formula
     Celsius_conversion = ( Celsius_input * 9/5) + 32
     print(Celsius_conversion , "F" )
-if UnitType == "F" or UnitType == "f" :
+
+#if it happens to be in Fahreinheit it must be converted to Celcious
+if UnitType.upper() == "F":
     Fahrenheit_input = input("What Degrees Fahrenheit is the temperature? ")
     Fahrenheit_input = int(Fahrenheit_input)
+    #display which type of temperature we are converting to
     print("Converting to Celsius!!!")
+    #conversion formula
     Fahrenheit_conversion = ( Fahrenheit_input - 32 ) * (5/9)
     print(Fahrenheit_conversion , "C")
